0e86b6205c2867511d6a068480d3d117de4a1d02
chainman2209 <chainsawman2209@gmail.com>
2023-11-19 14:59:04 +0700

    styles: color text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,35 +25,39 @@ export default function Home() {
                 />
                 <div className="absolute h-full w-full top-0 rounded-full border-4 border-blue-400 animate-spin"></div>
               </div>
-              <h1 className="font-semibold text-xl">Tran Thuy</h1>
+              <h1 className="font-semibold text-xl text-white">Tran Thuy</h1>
             </div>
             <div className="flex flex-col gap-2 mt-3">
               <div className="flex gap-2">
                 <PhoneCallIcon />
-                <a href="tel:+84376187961">(+84) 376 187 961</a>
+                <a href="tel:+84376187961" className="text-white">
+                  (+84) 376 187 961
+                </a>
               </div>
               <div className="flex gap-2">
                 <Mail />
-                <a href="mailto:">tranthanhthuy@gmail.com</a>
+                <a href="mailto:" className="text-white">
+                  tranthanhthuy@gmail.com
+                </a>
               </div>
             </div>
           </div>
 
           <div className="mt-3 relative">
             <div className="absolute bottom-[0.58rem] h-[1px] w-full bg-white z-10"></div>
-            <h2 className="relative text-center font-semibold z-20 w-20 bg-black mx-auto">
+            <h2 className="relative text-center font-semibold z-20 w-20 bg-black mx-auto text-white">
               Socials
             </h2>
           </div>
           <div className="flex justify-center items-center gap-3 mt-3">
             <Link href="https://www.facebook.com/Dev.Thuy.IT" target="_blank">
-              <Facebook />
+              <Facebook className="text-white" />
             </Link>
             <Link href="/" target="_blank">
-              <Instagram />
+              <Instagram className="text-white" />
             </Link>
             <Link href="/" target="_blank">
-              <Linkedin />
+              <Linkedin className="text-white" />
             </Link>
           </div>
         </div>
